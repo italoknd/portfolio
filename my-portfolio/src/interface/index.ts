387ecc1237e6fe16c392885dcd579f1ccc015c25
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2025-05-16 21:30:06 -0400

    configuring project

--- my-portfolio/src/interface/index.ts
+++ my-portfolio/src/interface/index.ts
@@ -1,7 +1,7 @@
 export interface ICardsInfos {
-    project_title: string
-    short_description:string
-    has_access_link: boolean
-    quick_access_link: string
-    project_on_github: string
-  },
+  project_title: string;
+  short_description: string;
+  has_access_link: boolean;
+  quick_access_link: string;
+  project_on_github: string;
+}
