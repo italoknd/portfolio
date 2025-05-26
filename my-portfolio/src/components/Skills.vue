41bf4df877d6da786e1ffcc53c9f9a6b8b462f82
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2025-05-26 01:21:53 -0400

    fix hover bug

--- my-portfolio/src/components/Skills.vue
+++ my-portfolio/src/components/Skills.vue
@@ -4,106 +4,78 @@ import { ref } from "vue";
 interface IStack {
   url: string;
   techName: string;
-  predominantColor: string;
-  showPredominantColor: boolean;
+  predominantColorOnHover: string;
 }
 
 let techs = ref<IStack[]>([
   {
     url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
     techName: "HTML",
-    predominantColor: "#E44D26",
-    showPredominantColor: false,
+    predominantColorOnHover: "hover:text-[#E44D26]",
   },
   {
     url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
     techName: "CSS 3",
-    predominantColor: "#1572B6",
-    showPredominantColor: false,
+    predominantColorOnHover: "hover:text-[#1572B6]",
   },
   {
     url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
     techName: "JavaScript",
-    predominantColor: "#F0DB4F",
-    showPredominantColor: false,
+    predominantColorOnHover: "hover:text-[#F0DB4F]",
   },
   {
     url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
     techName: "TypeScript",
-    predominantColor: "#007ACC",
-    showPredominantColor: false,
+    predominantColorOnHover: "hover:text-[#007ACC]",
   },
   {
     url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original.svg",
     techName: "Vue JS",
-    predominantColor: "#41B883",
-    showPredominantColor: false,
+    predominantColorOnHover: "hover:text-[#41B883]",
   },
   {
     url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
     techName: "React JS",
-    predominantColor: "#61DAFB",
-    showPredominantColor: false,
+    predominantColorOnHover: "hover:text-[#61DAFB]",
   },
   {
     url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
     techName: "Git",
-    predominantColor: "#F34F29",
-    showPredominantColor: false,
+    predominantColorOnHover: "hover:text-[#F34F29]",
   },
   {
     url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg",
     techName: "GitHub",
-    predominantColor: "#181616",
-    showPredominantColor: false,
+    predominantColorOnHover: "hover:text-[#181616]",
   },
 ]);
-
-let showPredominatColor = ref<boolean>(false);
 </script>
 
 <template>
   <section>
     <div>
       <div id="skills">
-        <h2 class="text-4xl font-semibold text-[var(--light-mode-composition-color)] mb-5">
+        <h2
+          class="text-4xl font-semibold text-[var(--light-mode-composition-color)] mb-5"
+        >
           Skills
         </h2>
-        <!-- <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
-          earum quia maxime animi necessitatibus, consequuntur doloremque sint
-          ducimus dolores eaque ipsa reiciendis molestias itaque aspernatur
-          aliquid temporibus voluptatum quidem ex. Lorem, ipsum dolor sit amet
-          consectetur adipisicing elit. Accusamus ex fugit minus dolorum quas
-          eos reprehenderit, commodi illum praesentium officiis quibusdam quis
-          veniam. Doloribus optio deleniti eveniet et similique illum.
-        </p> -->
       </div>
       <div>
-        <!-- <h2>Stack</h2> -->
         <div class="grid grid-cols-4 gap-5">
-          <div v-for="(tech, index) in techs"
-            class="cursor-pointer border rounded-md shadow-xl flex items-center justify-between p-4 hover:scale-105 duration-200"
-            @mouseenter="tech.showPredominantColor = true" @mouseleave="tech.showPredominantColor = false">
-            <img :key="index" :src="tech.url" class="object-cover w-[150px] h-[150px]" />
-            <p class="text-2xl font-semibold" :class="
-                tech.showPredominantColor
-                  ? `text-[${tech.predominantColor}]`
-                  : ``
-              ">
+          <div
+            v-for="(tech, index) in techs"
+            :key="index"
+            :class="`
+             ${tech.predominantColorOnHover} hover:scale-105
+             cursor-pointer border rounded-md shadow-xl flex items-center justify-between p-4 duration-200`"
+          >
+            <img :src="tech.url" class="object-cover w-[150px] h-[150px]" />
+            <p class="text-2xl font-semibold">
               {{ tech.techName }}
             </p>
           </div>
         </div>
-        <!-- <n-card
-          title="Card with Cover"
-          
-          :key="index"
-        >
-          <template #cover>
-          </template>
-          Card Content
-        </n-card> -->
       </div>
     </div>
   </section>
