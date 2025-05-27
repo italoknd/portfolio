4096aec7bc2149b150678b3b9d4819ef13ee1460
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2025-05-27 02:09:52 -0400

    salvar progresso

--- my-portfolio/src/components/Skills.vue
+++ my-portfolio/src/components/Skills.vue
@@ -5,48 +5,57 @@ interface IStack {
   url: string;
   techName: string;
   predominantColorOnHover: string;
+  isHovering: boolean;
 }
 
 let techs = ref<IStack[]>([
   {
     url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
     techName: "HTML",
-    predominantColorOnHover: "hover:text-[#E44D26]",
+    predominantColorOnHover: "hover:text-white hover:bg-[#F06529]/75",
+    isHovering: false,
   },
   {
     url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
     techName: "CSS 3",
-    predominantColorOnHover: "hover:text-[#1572B6]",
+    predominantColorOnHover: "hover:text-white hover:bg-[#2965f1]/75",
+    isHovering: false,
   },
   {
     url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
     techName: "JavaScript",
-    predominantColorOnHover: "hover:text-[#F0DB4F]",
+    predominantColorOnHover: "hover:text-white hover:bg-[#f7df1e]/65",
+    isHovering: false,
   },
   {
     url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
     techName: "TypeScript",
-    predominantColorOnHover: "hover:text-[#007ACC]",
+    predominantColorOnHover: "hover:text-white hover:bg-[#3178c6]/75",
+    isHovering: false,
   },
   {
     url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original.svg",
     techName: "Vue JS",
-    predominantColorOnHover: "hover:text-[#41B883]",
+    predominantColorOnHover: "hover:text-white hover:bg-[#42b883]/75",
+    isHovering: false,
   },
   {
     url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
     techName: "React JS",
-    predominantColorOnHover: "hover:text-[#61DAFB]",
+    predominantColorOnHover: "hover:text-white hover:bg-[#61DAFB]/75",
+    isHovering: false,
   },
   {
     url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
     techName: "Git",
-    predominantColorOnHover: "hover:text-[#F34F29]",
+    predominantColorOnHover: "hover:text-white hover:bg-[#F34F29]/75",
+    isHovering: false,
   },
   {
     url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg",
     techName: "GitHub",
-    predominantColorOnHover: "hover:text-[#181616]",
+    predominantColorOnHover: "hover:text-white hover:bg-[#181616]/75",
+    isHovering: false,
   },
 ]);
 </script>
@@ -56,33 +65,33 @@ let techs = ref<IStack[]>([
     <div>
       <div id="skills">
         <h2
-          class="text-4xl font-semibold text-[var(--light-mode-composition-color)] mb-5"
+          class="text-4xl text-center font-semibold text-[var(--light-mode-composition-color)] mb-5"
         >
           Skills
         </h2>
       </div>
       <div>
-        <div class="grid grid-cols-4 gap-5">
+        <div class="grid grid-cols-8 gap-5">
           <div
+            @mouseover="tech.isHovering = true"
+            @mouseleave="tech.isHovering = false"
             v-for="(tech, index) in techs"
             :key="index"
             :class="`
              ${tech.predominantColorOnHover} hover:scale-105
-             cursor-pointer border rounded-md shadow-xl flex items-center justify-between p-4 duration-200`"
+             cursor-pointer border rounded-md shadow-xl m-auto p-4 duration-200 `"
           >
-            <img :src="tech.url" class="object-cover w-[150px] h-[150px]" />
-            <p class="text-2xl font-semibold">
-              {{ tech.techName }}
-            </p>
+          <p class="text-2xl font-semibold text-center mb-2">
+            {{ tech.techName }}
+          </p>
+            <img
+              :src="tech.url"
+              class="object-cover w-[150px] h-[150px] duration-200"
+              :class="tech.isHovering ? 'scale-75 ' : ' scale-100'"
+            />
           </div>
         </div>
       </div>
     </div>
   </section>
 </template>
-<style scoped>
-.n-card {
-  max-width: 300px;
-  max-height: 300px;
-}
-</style>
