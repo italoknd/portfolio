2de54ca69f4bd24ad466e7eec9d751a193440054
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2025-06-07 13:59:58 -0400

    resolving build errors

--- my-portfolio/src/views/Portfolio.vue
+++ my-portfolio/src/views/Portfolio.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import AboutMe from "../components/AboutMe.vue";
 import Skills from "../components/Skills.vue";
-import MyProjects from "../components/MyProjects.vue";
-import Contact from "../components/Contact.vue";
+// import MyProjects from "../components/MyProjects.vue";
+// import Contact from "../components/Contact.vue";
 </script>
 <template>
   <div>
