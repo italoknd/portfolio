9a4291ccf70ce15056e10c55a419d36679a59eb1
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2025-06-11 02:44:06 -0400

    fixing navbar on mobile

--- my-portfolio/src/components/Navbar.vue
+++ my-portfolio/src/components/Navbar.vue
@@ -22,9 +22,9 @@ const openSideMenu = (stts: boolean) => {
 </script>
 
 <template>
-  <n-page-header class="p-5">
+  <n-page-header class="p-3 md:p-5 w-full z-20 fixed bg-white shadow-xl">
     <div class="flex justify-between items-center">
-      <h2 class="text-4xl font-semibold text-gray-800">
+      <h2 class="text-2xl md:text-4xl font-semibold text-gray-800">
         Port<span class="text-[var(--light-mode-composition-color)]"
           >folio
         </span>
