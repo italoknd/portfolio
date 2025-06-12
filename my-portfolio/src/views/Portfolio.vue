51a330be3eddccb2251e3afeb9e77bdad111e918
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2025-06-11 23:17:36 -0400

    fix scrollbar track background

--- my-portfolio/src/views/Portfolio.vue
+++ my-portfolio/src/views/Portfolio.vue
@@ -1,17 +1,17 @@
 <script setup lang="ts">
 import AboutMe from "../components/AboutMe.vue";
 import Skills from "../components/Skills.vue";
-// import MyProjects from "../components/MyProjects.vue";
+import MyProjects from "../components/MyProjects.vue";
 // import Contact from "../components/Contact.vue";
 </script>
 <template>
   <div>
-    <div class="border-[0.5px] border-gray-600"></div>
     <AboutMe class="my-12 p-4" />
     <div class="border-[0.5px] border-gray-600"></div>
     <Skills class="p-4" />
+    <div class="border-[0.5px] border-gray-600"></div>
+    <MyProjects class="p-4" />
     <!-- 
-    <MyProjects />
     <Contact /> -->
   </div>
 </template>
